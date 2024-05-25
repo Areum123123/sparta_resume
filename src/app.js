8179abf53e2716102f62d1d6647cb40a070ba331
areum <kang10232@naver.com>
2024-05-25 19:10:47 +0900

    feat:회원가입 API 구현

--- src/app.js
+++ src/app.js
@@ -1,6 +1,22 @@
 import express from 'express';
-import {prisma} from './utils/prisma.util.js'; //사용처.. 확인
-import { errorHandler } from './middlewares/error-handler.middleware';
+import cookieParser from 'cookie-parser';
+import { errorHandler } from './middlewares/error-handler.middleware.js';
+import  UsersRouter  from './routers/users.routers.js'
+
+const app = express();
+const PORT =3018;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use('/api',[UsersRouter])
+
+
+app.use(errorHandler); //error
+
+app.listen(PORT,()=>{
+    console.log(PORT,'포트로 서버가 열렸습니다!')
+});
+
 
 
 
---
+++ src/routers/users.routers.js
@@ -0,0 +1,90 @@
+import express from "express";
+import bcrypt from "bcrypt";
+import {prisma} from '../utils/prisma.util.js'; //사용처.. 확인
+
+
+const router = express.Router()
+
+/*회원가입 API */
+router.post('/register', async(req, res, next)=>{ 
+   // - **이메일, 비밀번호, 비밀번호 확인, 이름**을 **Request Body**(**`req.body`**)
+   const {email, password, passwordConfirm, name} =req.body;
+//2. **유효성 검증 및 에러 처리**
+//- **이메일이 중복되는 경우** - “이미 가입 된 사용자입니다.”
+try{
+ const isExistUser = await prisma.users.findFirst({
+    where:{
+        email,
+    },
+ })
+ if(isExistUser){
+    return res.status(409).json({
+        status:409,
+        message:"이미 가입 된 사용자입니다."                      
+    })
+ }
+   // - **회원 정보 중 하나라도 빠진 경우** - “OOO을 입력해 주세요.”
+   if(!email || !name || !password ||!passwordConfirm){
+     const missingFields =[];
+     if(!email){missingFields.push('이메일을');}
+     if(!name){missingFields.push('이름을');}
+     if(!password){missingFields.push('비밀번호를');}
+     if(!passwordConfirm){missingFields.push('비밀번호 확인을');}
+      
+
+    return res.status(400).json({
+        status:400,
+        message:`${missingFields} 입력해 주세요.`
+    })
+   }
+   // - **이메일 형식에 맞지 않는 경우** - “이메일 형식이 올바르지 않습니다.”
+   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+   if(!emailRegex.test(email)){
+    return res.status(400).json({
+        status: 400,
+        message: "이메일 형식이 올바르지 않습니다."
+    });
+   }
+   
+   // - **비밀번호가 6자리 미만인 경우** - “비밀번호는 6자리 이상이어야 합니다.”
+   if(password.length < 6){
+    return res.status(400).json({
+        status: 400,
+        message: "비밀번호는 6자리 이상이어야 합니다."
+     });
+   }
+   /// - **비밀번호와 비밀번호 확인이 일치하지 않는 경우** - “입력 한 두 비밀번호가 일치하지 않습니다.”
+   if(password !== passwordConfirm){
+    return res.status(400).json({
+         status:400,
+         message:"입력 한 두 비밀번호가 일치하지 않습니다."
+    });
+   }
+//3. **비즈니스 로직(데이터 처리)**
+    //- 보안을 위해 **비밀번호**는 평문(Plain Text)으로 저장하지 않고 **Hash 된 값**을 저장합니다.
+    const hashedPassword = await bcrypt.hash(password, 10);
+//Users테이블에 사용자를 추가
+const user = await prisma.users.create({
+    data:{
+        email,
+        password:hashedPassword,
+        name,
+    },
+})
+
+return res.status(201).json({
+    message: "회원 가입이 성공적으로 완료되었습니다."
+  })
+} catch(err){
+    next(err);
+  }
+})
+
+
+
+
+
+
+
+
+export default router;
